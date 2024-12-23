56b942bca7a56a10e7366cfad9c74efcc018ada6
codeftsaikat <soikatdey18@gmail.com>
2024-12-23 04:12:26 -0800

    Implement side bar for our project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/home/HomePage"
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import Sidebar from "./components/sidebar/Sidebar";
 
-function App() {  
-
+function App() {
   return (
-    <div>
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* Bg */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80"/>
+        <div className="absolute inset-0 backdrop-blur-sm"/>
+      </div>
+
+      <Sidebar/>
+
+      {/* Routing */}
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
